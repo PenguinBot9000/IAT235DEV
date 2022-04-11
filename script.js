7569a800d1cb6916b2ca34f09412f135b118c637
PenguinBot9000 <103341856+PenguinBot9000@users.noreply.github.com>
2022-04-11 01:27:07 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const element = document.querySelector('.img');
 const scrollAmount = -2;
 window.addEventListener('scroll', (event) =>{
 	const {top} = element.getBoundingClientRect();
-
+  const {bottom} = element.getBoundingClientRect();
   if( top - window.innerHeight < scrollAmount){
   element.style.backgroundImage = 'url(Img/stevenholl.png)';
-  }
-  else(top -window.innerHeight > scrollAmount){
+  }else if(bottom - window.innerHeight > scrollAmount){
     element.style.backgroundImage = 'url(Img/a.jpg)';
   }
+  
 });
 };
 
